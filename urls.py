4b0abbfcde4ecfaecba37de2f68ce90f1caf7a2e
ashutosh762 <80394618+ashutosh762@users.noreply.github.com>
2021-05-12 12:27:42 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,22 +1,16 @@
-"""sporto URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('sportocrats.urls')),
+    path('',views.index, name='index'),
+    path('main1.html', views.main1, name='main1'),
+    path('aboutus.html', views.aboutus, name='aboutus'),
+    path('announce.html', views.announce, name='announce'),
+    path('contactus.html', views.contactus, name='contactus'),
+    path('register.html', views.register, name='register'),
+    path('male.html', views.male, name='male'),
+    path('female.html', views.female, name='female'),
+
 ]
--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 
 # Create your views here.
-from .models import Player_Male
+from .models import Player_Male,Player_female,admin_sports
 
 
 def index(request):
@@ -20,19 +20,32 @@ def contactus(request):
     return render(request, 'contactus.html')
 
 def register(request):
-    return render(request, 'register.html')
+    if request.method=='POST':
+        first_name=request.POST['name1']
+        last_name=request.POST['name2']
+        email = request.POST['email']
+        password = request.POST['password']
+        sports = request.POST['sports']
+        post=request.POST['sports']
+
+        ins=admin_sports(first_name=first_name,last_name=last_name,email=email,password=password,sports=sports,post=post)
+        ins.save()
+        print("user created")
+        return redirect('/')
+    else:
+        return render(request, 'register.html')
 
 
 def male(request):
     if request.method=='POST':
         first_name=request.POST['name1']
         last_name=request.POST['name2']
-        dob = request.POST['dob']
+        sid = request.POST['sid']
         email = request.POST['email']
         password = request.POST['password']
         sports = request.POST['sports']
 
-        ins=Player_Male(first_name=first_name,last_name=last_name,dob=dob,email=email,password=password,sports=sports)
+        ins=Player_Male(first_name=first_name,last_name=last_name,sid=sid,email=email,password=password,sports=sports)
         ins.save()
         print("user created")
         return redirect('/')
@@ -50,4 +63,18 @@ def male(request):
 
 
 def female(request):
-    return render(request, 'female.html')
+    if request.method=='POST':
+        first_name=request.POST['name1']
+        last_name=request.POST['name2']
+        sid = request.POST['sid']
+        email = request.POST['email']
+        password = request.POST['password']
+        sports = request.POST['sports']
+
+        ins=Player_female(first_name=first_name,last_name=last_name,sid=sid,email=email,password=password,sports=sports)
+        ins.save()
+        print("user created")
+        return redirect('/')
+
+    else:
+        return render(request, 'female.html')
